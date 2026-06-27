2ad65be24eec2038a21d42c710807721cdcb21a5
shelly004 <nemashelly99@gmil.com>
2026-06-27 19:58:13 +0530

    Form data validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 import SearchData from './components/Datafetch-debounce/SearchData'
+import Form from './components/Form-validation/Form'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <SearchData/>
+    <>
+      <SearchData/>
+      <Form/>
+    </>
+   
   )
 }
 
